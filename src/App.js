04938ebacbdd84afcb165e8da0c712db4fe0088e
Shailav Malik <malikshailav@gmail.com>
2023-12-24 11:55:05 +0530

    slicing a string

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Emojipedia from "./emojipedia";
 
 function App() {
+  const arr = [];
+  Emojipedia.map(function (obj) {
+    arr.push(obj.meaning.slice(0, 100));
+  });
+
+  console.log(arr);
+
   return <></>;
 }
 
